77ad35a58278541bf8316dbe1eaffd7ed8ca2389
Peter Burns <rictic@gmail.com>
2025-06-26 02:09:55 -0700

    Test change: Update hello server message

--- nixos-wsl/base-configuration.nix
+++ nixos-wsl/base-configuration.nix
@@ -459,7 +459,7 @@ in
                 self.send_response(200)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
-                self.wfile.write(f'Hello from {hostname} - auto-update test!\n'.encode())
+                self.wfile.write(f'Hello from {hostname} - auto-update test UPDATED!\n'.encode())
             
             def log_message(self, format, *args):
                 pass  # Suppress default logging

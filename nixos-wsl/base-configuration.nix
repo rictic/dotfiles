525cc4749ace2941e1843027c1343a9969002781
Peter Burns <rictic@gmail.com>
2025-06-26 01:23:37 -0700

    Fix the hello hostname webserver.

--- nixos-wsl/base-configuration.nix
+++ nixos-wsl/base-configuration.nix
@@ -380,27 +380,28 @@ in
       Type = "simple";
       User = "rictic";
       WorkingDirectory = "/home/rictic";
-      ExecStart = "${pkgs.python3}/bin/python3 -c \"
-import http.server
-import socketserver
-import socket
+      ExecStart = "${pkgs.writeScript "hello-server.py" ''
+        #!${pkgs.python3}/bin/python3
+        import http.server
+        import socketserver
+        import socket
 
-class HelloHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        hostname = socket.gethostname()
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(f'Hello from {hostname}!\\n'.encode())
-    
-    def log_message(self, format, *args):
-        pass  # Suppress default logging
+        class HelloHandler(http.server.BaseHTTPRequestHandler):
+            def do_GET(self):
+                hostname = socket.gethostname()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(f'Hello from {hostname}!\n'.encode())
+            
+            def log_message(self, format, *args):
+                pass  # Suppress default logging
 
-PORT = 9876
-with socketserver.TCPServer(('', PORT), HelloHandler) as httpd:
-    print(f'Serving at port {PORT}')
-    httpd.serve_forever()
-\"";
+        PORT = 9876
+        with socketserver.TCPServer(('0.0.0.0', PORT), HelloHandler) as httpd:
+            print(f'Serving at port {PORT}')
+            httpd.serve_forever()
+      ''}";
       Restart = "always";
       RestartSec = "5";
     };
